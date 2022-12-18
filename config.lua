9aaf33e15c67c0628f11edaf5b72ab6e0df64f61
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2022-12-18 18:20:06 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,5 @@
+
+Config = {}
+
+Config.Strength = 1.0
+Config.Duration = 1000 
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'nikartik'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
